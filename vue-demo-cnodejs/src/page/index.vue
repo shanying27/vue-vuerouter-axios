0db3f5f156095e37d4fbb684fab4271b9bb30e60
shanying27 <13520192263@163.com>
2017-09-01 10:52:14 +0800

    first
    first create vue

---
+++ vue-demo-cnodejs/src/page/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+  <Header></Header>
+    <div>
+        <ul>
+            <li v-for="list in lists">
+            <router-link :to="'/content/' + list.id">{{ list.title }}</router-link>
+            </li>
+        </ul>
+    </div>
+  <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '../components/header.vue'
+import Footer from '../components/footer.vue'
+export default {
+  components: { Header, Footer },
+  data () {
+    return {
+      lists: []
+    }
+  },
+  created () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      this.$api.get('main?name=dcg&type=news', null, r => {
+        console.log(r.data)
+        this.lists = r.data
+      })
+    }
+  }
+}
+</script>
