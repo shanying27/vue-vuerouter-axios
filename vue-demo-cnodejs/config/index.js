721e22cd33279b4ef2d6fd6972750ffd8044f902
shanying27 <13520192263@163.com>
2017-09-05 15:35:38 +0800

    配置API代理

--- vue-demo-cnodejs/config/index.js
+++ vue-demo-cnodejs/config/index.js
@@ -27,7 +27,13 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/dc/**': {
+      target: 'http://10.0.2.222:8088', // 你接口的域名
+      secure: false,
+      changeOrigin: false,
+  }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
